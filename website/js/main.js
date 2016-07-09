a00654212a5087c670979c5cafcb21929622e4b9
Nicolás Santángelo <santangelonicolas@gmail.com>
2016-07-09 11:02:03 -0300

    Feedback

--- website/js/main.js
+++ website/js/main.js
@@ -1,4 +1,16 @@
 (function () {
+  var isChromium = window.chrome
+  var vendorName = window.navigator.vendor
+  var isOpera    = window.navigator.userAgent.indexOf("OPR") > -1
+  var isIEedge   = window.navigator.userAgent.indexOf("Edge") > -1
+
+  if(isChromium !== null && isChromium !== undefined && vendorName === "Google Inc." && isOpera == false && isIEedge == false) {
+    // Browser detecting like is 1999
+    console.log('Hey!, look ↑, some versions of Chrome tend to change the frame for no reason, make sure it says top')
+  }
+})();
+
+(function () {
   var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
 
   var $navAnchor = jQuery('#js-nav-anchor')
@@ -7,14 +19,15 @@
   }
 
   $navAnchor.removeClass('invisible')
-})()
+})();
 
 jQuery(document).one('finished.weaintfoundshit', function () {
   var space = Array(34).join(' ')
   var styles = "padding:4px 0px;font-size:12px;font-family:Consolas;color:#242729;background-color:#e1e3e5;"
 
   console.log("Wasn't that %cawesome?", "color:green;")
-  console.log("You can change the source/type of the video using\n ")
+  console.log('I know what you are thinking, "how much free time does this guy have?" and "what if I want another video?"')
+  console.log('Well, You can change the source/type of the video changing\n')
   console.log("%c" + space, styles)
   console.log("%c  window.WEAINTFOUND_VIDEO_URL   ", styles)
   console.log("%c  window.WEAINTFOUND_VIDEO_TYPE  ", styles)
